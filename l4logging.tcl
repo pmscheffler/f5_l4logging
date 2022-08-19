d089544f62927ea6007b94f71be346c28587f3cd
Peter Scheffler <p.scheffler@f5.com>
2022-08-19 10:48:53 -0400

    first commit

---
+++ l4logging.tcl
@@ -0,0 +1,40 @@
+when RULE_INIT priority 100 {
+    set debuglevel 1
+}
+
+when CLIENT_ACCEPTED priority 100 {
+
+    set HSL-POOL syslog-pool
+
+    # could store the pool name in a Data Group
+    # along with the protocol
+    set hsl [HSL::open -proto UDP -pool $HSL-POOL]
+
+    set client_logentry "Received a request from Client [IP::client_addr]:[TCP::client_port] to the VIP [IP::local_addr]:[TCP::local_port]"
+    if {$debuglevel eq 1} 
+        log local0. "$client_logentry"
+}
+
+when LB_SELECTED priority 100 {
+    set lb_logentry "Server [IP::server_addr]:[TCP::server_port]" 
+    if {debuglevel eq 1} 
+        log local0. "$lb_logentry"
+}
+
+when LB_FAILED priority 100 {
+    set lb_logentry "LB Selection Failed"
+    if {debuglevel eq 1} 
+        log local0. "$lb_logentry"
+    
+    HSL::send $hsl $lb_logentry
+}
+
+when SERVER_CONNECTED priority 100 {
+
+    HSL::send $hsl "$client_logentry $lb_logentry"
+    if {debuglevel eq 1} 
+        log local0. "HSL sent"
+
+}
+
+
